f3f7bd6419b553b52885b2de68545389e1378c7b
shubhansh-simple <shubhansh7777@gmail.com>
2024-02-01 10:04:40 +0530

    Adding logo & Work experience increase section by section

--- files/script.js
+++ files/script.js
@@ -1,1 +1,27 @@
-console.log('I am in');
+function changeBorderColor( idVal ){
+
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      var element = document.getElementById(idVal)
+      console.log('Td is - ', element, idVal);
+
+      if (element) {
+        element.style.borderLeft = '4px solid #0d6efd';
+      }
+      resolve();
+    }, 500);
+  });
+}
+
+async function delaySetColors(){
+  IdList = ['one','two','three','four']
+  for ( let i=0; i< IdList.length; i++ ){
+    await changeBorderColor(IdList[i]);
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  console.log('DOM is ready');
+  delaySetColors();
+});
